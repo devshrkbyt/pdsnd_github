3a0e9e05f4e39ee9a14194a8c5b7b21d2a00a788
Max N <shrkbyt@gmail.com>
2020-03-12 13:29:05 +0200

    Added Refactoring comment

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -3,6 +3,8 @@ import time
 import pandas as pd
 import numpy as np
 
+#Add refactoring
+
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
              'washington': 'washington.csv'}
